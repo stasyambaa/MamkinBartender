1dfde8acd3186b83a7ff604262076caf79dd4ac3
stasyambaa <stas.shapetko93@gmail.com>
2022-04-16 14:41:58 +0200

    custom table view cell

---
+++ Мамкин Бармен/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  Мамкин Бармен
+//
+//  Created by Станислав Шапетько on 16.04.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageCocktail: UIImageView!
+    @IBOutlet weak var labelCocktail: UILabel!
+    
+}
